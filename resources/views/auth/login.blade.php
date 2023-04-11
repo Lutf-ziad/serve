f7f28f49ea0b2990d2750d96fb07350a4de85950
Lutf-ziad <lotfabdhziad@gmail.com>
2023-04-11 00:08:22 -0900

    this second in master

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,91 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>تسجيل الدخول</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="{{ asset('admin/fonts/ionicons/2.0.1/css/ionicons.min.css') }}">
+      <!-- icheck bootstrap -->
+      <link rel="stylesheet" href="{{ asset('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css') }}">
+      <!-- Google Font: Source Sans Pro -->
+      <link rel="stylesheet" href="{{ asset('admin/fonts/SansPro/SansPro.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('admin/css/bootstrap_rtl-v4.2.1/bootstrap.min.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/css/bootstrap_rtl-v4.2.1/custom_rtl.css')}}">
+      <style>
+         .login-box-msg, .register-box-msg {
+         margin: 0;
+         padding: 0 20px 20px;
+         text-align: center;
+         color: brown;
+         font-size: 1.5vw;
+         }
+         span.fas {
+         color: brown;
+         }
+      </style>
+   </head>
+   <body class="hold-transition login-page" style="background-image: url({{ asset('admin/imgs/login.jpg') }}) ;background-size:cover;background-repeate:ni-repeate; min-height:600px;">
+      <div class="login-box">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+         <!-- /.login-logo -->
+         <div class="card">
+            <div class="card-body login-card-body">
+               @if(Session::has('error'))
+               <div class="alert alert-danger" role="alert">
+                  {{  Session::get('error') }}
+               </div>
+               @endif
+               <p class="login-box-msg">تسجيل الدخول</p>
+               <form method="POST" action="{{route('login') }}">
+                @csrf
+                  <div class="input-group mb-3">
+                     <input  type="text" name="email" class="form-control" value="{{ old('email') }}" placeholder="email">
+                     <div class="input-group-append">
+                        <div class="input-group-text">
+                           <span class="fas fa-envelope"></span>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                     </div>
+                  </div>
+                  @error('email')
+                  <span class="text-danger">{{ $message }}</span>
+                  @enderror
+                  <div class="input-group mb-3">
+                     <input type="password" name="password" class="form-control" value="{{ old('password') }}" placeholder="Password">
+                     <div class="input-group-append">
+                        <div class="input-group-text">
+                           <span class="fas fa-lock"></span>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                     </div>
+                  </div>
+                  @error('password')
+                  <span class="text-danger">{{ $message }}</span>
+                  @enderror
+                  <div class="row">
+                     <!-- /.col -->
+                     <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">دخول </button>
+                     </div>
+                     <a class="btn btn-link" href="{{ route('register') }}">
+                        انشا حساب
+                    </a>
+                     <!-- /.col -->
+                  </div>
+               </form>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+            <!-- /.login-card-body -->
+         </div>
+      </div>
+      <!-- /.login-box -->
+      <!-- jQuery -->
+      <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+      <!-- Bootstrap 4 -->
+      <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+   </body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,111 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>تسجيل حساب</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="{{ asset('admin/fonts/ionicons/2.0.1/css/ionicons.min.css') }}">
+      <!-- icheck bootstrap -->
+      <link rel="stylesheet" href="{{ asset('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css') }}">
+      <!-- Google Font: Source Sans Pro -->
+      <link rel="stylesheet" href="{{ asset('admin/fonts/SansPro/SansPro.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('admin/css/bootstrap_rtl-v4.2.1/bootstrap.min.css')}}">
+      <link rel="stylesheet" href="{{ asset('admin/css/bootstrap_rtl-v4.2.1/custom_rtl.css')}}">
+      <style>
+         .login-box-msg, .register-box-msg {
+         margin: 0;
+         padding: 0 20px 20px;
+         text-align: center;
+         color: brown;
+         font-size: 1.5vw;
+         }
+         span.fas {
+         color: brown;
+         }
+      </style>
+   </head>
+   <body class="hold-transition login-page" style="background-image: url({{asset('admin/imgs/login.jpg') }}) ;background-size:cover;background-repeate:ni-repeate; min-height:600px;">
+      <div class="login-box">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+         <!-- /.login-logo -->
+         <div class="card">
+            <div class="card-body login-card-body">
+               @if(Session::has('error'))
+               <div class="alert alert-danger" role="alert">
+                  {{  Session::get('error') }}
+               </div>
+               @endif
+               <p class="login-box-msg">   انشا حساب </p>
+               <form method="POST" action="{{ route('register') }}">
+                @csrf
+                  <div class="input-group mb-3">
+                     <input  type="text" name="name" value="{{ old('name') }}" class="form-control"  >
+                     <div class="input-group-append">
+                        <div class="input-group-text">
+                           <span class="fas fa-envelope"></span>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                     </div>
+                  </div>
+                  @error('username')
+                  <span class="text-danger">{{ $message }}</span>
+                  @enderror
+                  <div class="input-group mb-3">
+                     <input  id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" >
+                     <div class="input-group-append">
+                        <div class="input-group-text">
+                           <span class="fas fa-envelope"></span>
                         </div>
+                     </div>
+                  </div>
+                  @error('email')
+                  <span class="text-danger">{{ $message }}</span>
+                  @enderror
+                  <div class="input-group mb-3">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                     <input type="password" name="password" value="{{ old('password') }}" class="form-control">
+                     <div class="input-group-append">
+                        <div class="input-group-text">
+                           <span class="fas fa-lock"></span>
                         </div>
+                     </div>
+                  </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                  <div class="input-group mb-3">
+
+                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" >
+                     <div class="input-group-append">
+                        <div class="input-group-text">
+                           <span class="fas fa-lock"></span>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                     </div>
+                  </div>
+                  @error('password_confirmation')
+                  <span class="text-danger">{{ $message }}</span>
+                  @enderror
+                  <div class="row">
+                     <!-- /.col -->
+                     <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">دخول </button>
+                     </div>
+                     <!-- /.col -->
+                  </div>
+               </form>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+            <!-- /.login-card-body -->
+         </div>
+      </div>
+      <!-- /.login-box -->
+      <!-- jQuery -->
+      <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+      <!-- Bootstrap 4 -->
+      <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+   </body>
+</html>
